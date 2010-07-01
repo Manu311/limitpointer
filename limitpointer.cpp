b33cae24a09230bf333b8cc5fadf4534a3fd1d17
Manu <Manuel.Bihl@gmx.de>
2010-07-01 18:04:08 +0200

    programm didn't work on my local machine removed XNextEvent and replaced it with an 0.5 ms sleep (waste of cpu power, but working)

--- limitpointer.cpp
+++ limitpointer.cpp
@@ -6,20 +6,21 @@ int main ()
 	Display *dsp = XOpenDisplay( NULL );
 	if( !dsp ){ return 1; }
 
-	int screen_width = DisplayWidth(dsp, DefaultScreen(dsp));
+	int screen_width = DisplayWidth(dsp, DefaultScreen(dsp)) / 2;
 	
 	Window focusWin = RootWindow(dsp, DefaultScreen(dsp));
 
 	XEvent event;
 
-	XSelectInput(dsp, focusWin, PointerMotionMask);
+	//XSelectInput(dsp, focusWin, PointerMotionMask);
 
 	while (true)
 	{
-		XNextEvent(dsp, &event);
+		//XNextEvent(dsp, &event);
 		XQueryPointer(dsp, focusWin, &event.xbutton.root, &event.xbutton.window, &event.xbutton.x_root, &event.xbutton.y_root, &event.xbutton.x, &event.xbutton.y, &event.xbutton.state);
 		if (event.xbutton.x >= screen_width)
-			XTestFakeMotionEvent(dsp, DefaultScreen(dsp), screen_width, event.xbutton.y, 0);
+			XTestFakeMotionEvent(dsp, DefaultScreen(dsp), screen_width - 1, event.xbutton.y, 0);
+		usleep(500); //0.5 ms
 	}
 
 	XCloseDisplay( dsp );
